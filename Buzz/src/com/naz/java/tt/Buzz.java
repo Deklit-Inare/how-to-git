c558d5b103890b4bf3a3bc4a2ba56bfd91156e4b
Ágoston Németh <nemagost@gmail.com>
2017-11-17 16:22:56 +0100

    Buzz

--- Buzz/src/com/naz/java/tt/Buzz.java
+++ Buzz/src/com/naz/java/tt/Buzz.java
@@ -1,13 +1,12 @@
 package com.naz.java.tt;
 
-public class FizzMain {
+public class Buzz {
 
 	public static void main(String[] args) {
 
-		for (int i = 1; i <= 100; i++) {
-
-			if (i % 3 == 0) {
-				System.out.println("Fizz");
+		for (int i = 0; i < 100; i++) {
+			if (i % 4 == 0) {
+				System.out.println("Buzz");
 			} else {
 				System.out.println(i);
 			}

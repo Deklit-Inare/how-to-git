e5b069351d68857bb7ff63dd5d3c71485b979f38
Ágoston Németh <nemagost@gmail.com>
2017-11-17 16:09:34 +0100

    Modified the code so that it prints "Fizz" for every third number.

--- Fizz/src/com/naz/java/tt/FizzMain.java
+++ Fizz/src/com/naz/java/tt/FizzMain.java
@@ -3,15 +3,16 @@ package com.naz.java.tt;
 public class FizzMain {
 
 	public static void main(String[] args) {
-		
-		for(int i = 0; i < 100; i++) {
-			
-			System.out.println(i);
-			
+
+		for (int i = 1; i <= 100; i++) {
+
+			if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else {
+				System.out.println(i);
+			}
 		}
-		
-		System.out.println("Printed the first 100 numbers.");
-		
+
 	}
 
 }
